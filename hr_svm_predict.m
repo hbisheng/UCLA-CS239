f76755096254ecd4d5dd301570c9af8ea050f7fe
Zhengrong Wang <seanyukigeek@gmail.com>
2017-06-13 10:02:42 -0700

    finalize

--- hr_svm_predict.m
+++ hr_svm_predict.m
@@ -1,7 +1,7 @@
 function [predicted, RMSE] = hr_svm_predict(data, fs, hr, model)
     % Simply aggregate them.
-    data = sqrt(data(:,:,1).^2 + data(:,:,2).^2 + data(:,:,3).^2);
-    features = fq_feature_extract(data, fs);
+    agg = sqrt(data(:,:,1).^2 + data(:,:,2).^2 + data(:,:,3).^2);
+    features = fq_feature_extract(agg, fs);
     % Predict.
     predicted = predict(model, features);
     RMSE = computeRMSE(predicted, hr);
@@ -9,8 +9,17 @@ function [predicted, RMSE] = hr_svm_predict(data, fs, hr, model)
     % Plot the prediction result
     figure;
     scatter(hr, predicted);
+    hold on;
+    plot(hr, hr, 'LineWidth', 2);
 %     plot(hr);
 %     hold on;
 %     plot(predicted);
-    legend('Response in Testing data','Predicted Response','location','best');
+    xlabel('Goundtrue HR (BPM)');
+    ylabel('Estimated HR (BPM)');
+    window = size(agg, 2) / fs;
+    sensor = 'Accelerometer';
+    if fs < 200
+        sensor = 'Gyroscope';
+    end
+    title(sprintf('Estimated HR (BPM), window = %ds, %s', window, sensor));
 end
